ec7260b519d6cfa5bb217b624eec74411e27982c
hglong16 <intihad.vuong@gmail.com>
2022-06-29 09:34:37 +0700

    sua loi chinh ta

--- local-addons/merctrans_projects/models/merctrans_projects.py
+++ local-addons/merctrans_projects/models/merctrans_projects.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+
+from odoo import api, fields, models
 
 
 class MercTransServices(models.Model):
@@ -8,8 +9,11 @@ class MercTransServices(models.Model):
     _rec_name = 'services_names'
     _description = 'Services offered by MercTrans'
 
-    department_list = [('localization', 'Localization'), ('marketing', 'Marketing'), ('developement', 'Development')]
-    department = fields.Selection(string="Department", selection=department_list)
+    department_list = [('localization', 'Localization'),
+                       ('marketing', 'Marketing'),
+                       ('developement', 'Development')]
+    department = fields.Selection(string="Department",
+                                  selection=department_list)
     services_names = fields.Char('Services')
 
 
@@ -18,29 +22,44 @@ class MercTransProjects(models.Model):
     _description = 'MercTrans Projects'
     _rec_name = 'job_name'
 
-    language_list = [('zh-CN', 'Chinese (Simplified)'), ('zh-TW', 'Chinese (Traditional)'), ('en-US', 'English   (US)'),
-                     ('en-GB', 'English (UK)'), ('fr-FR', 'French'), ('it-IT', 'Italian'), ('es-ES', 'Spanish (Spain)'),
-                     ('es-AR', 'Spanish (Argentina)'), ('es-LA', 'Spanish (Latin America)'), ('th-TH', 'Thai'),
-                     ('tr-TR', 'Turkish'), ('vi-VN', 'Vietnamese'), ('ko-KR', 'Korean'), ('ja-JP', 'Japanese'),
-                     ('ru-RU', 'Russian'), ('de-DE', 'German (Germany)'), ('de-AT', 'German (Austria)'),
+    language_list = [('zh-CN', 'Chinese (Simplified)'),
+                     ('zh-TW', 'Chinese (Traditional)'),
+                     ('en-US', 'English   (US)'), ('en-GB', 'English (UK)'),
+                     ('fr-FR', 'French'), ('it-IT', 'Italian'),
+                     ('es-ES', 'Spanish (Spain)'),
+                     ('es-AR', 'Spanish (Argentina)'),
+                     ('es-LA', 'Spanish (Latin America)'), ('th-TH', 'Thai'),
+                     ('tr-TR', 'Turkish'), ('vi-VN', 'Vietnamese'),
+                     ('ko-KR', 'Korean'), ('ja-JP', 'Japanese'),
+                     ('ru-RU', 'Russian'), ('de-DE', 'German (Germany)'),
+                     ('de-AT', 'German (Austria)'),
                      ('de-CH', 'German (Switzerland)')]
 
-    work_unit_list = [('word', 'Word'), ('hour', 'Hour'), ('page', 'Page'), ('job', 'Job')]
+    work_unit_list = [('word', 'Word'), ('hour', 'Hour'), ('page', 'Page'),
+                      ('job', 'Job')]
 
-    project_status_list = [('potential', 'Potential'), ('confirmed', 'Confirmed'), ('in progress', 'In Progress'),
-                           ('in qa', 'In QA'), ('delivered', 'Delivered'), ('canceled', 'Canceled')]
+    project_status_list = [('potential', 'Potential'),
+                           ('confirmed', 'Confirmed'),
+                           ('in progress', 'In Progress'), ('in qa', 'In QA'),
+                           ('delivered', 'Delivered'),
+                           ('canceled', 'Canceled')]
 
-    payment_status_list = [('unpaid', 'Unpaid'), ('invoiced', 'Invoiced'), ('paid', 'Paid')]
+    payment_status_list = [('unpaid', 'Unpaid'), ('invoiced', 'Invoiced'),
+                           ('paid', 'Paid')]
 
-    job_id = fields.Integer('Project ID')
-    job_name = fields.Char('Project Name', default='Project Name')
+    project_id = id
+    project_name = fields.Char('Project Name', default='Project Name')
     client = fields.Many2many('res.partner', string='Clients', required=True)
 
     # services contain tags
     services_ids = fields.Many2many('merctrans.services', string='Services')
-    job_instruction = fields.Char('Project Instruction')
-    source_language = fields.Selection(string="Source Languages", selection=language_list, default="Select a language")
-    target_language = fields.Selection(string="Target Language", selection=language_list, default="Select a language")
+    project_instruction = fields.Char('Project Instruction')
+    source_language = fields.Selection(string="Source Languages",
+                                       selection=language_list,
+                                       default="Select a language")
+    target_language = fields.Selection(string="Target Language",
+                                       selection=language_list,
+                                       default="Select a language")
     discount = fields.Integer('Discount (%)')
     # add discount field
     # fixed job
@@ -50,14 +69,19 @@ class MercTransProjects(models.Model):
     currency_id = fields.Many2one('res.currency', string='Currency')
     sale_rate_per_work_unit = fields.Float('Sale rate per Work Unit')
     # production_rate_per_work_unit = fields.Float('Production rate per Work Unit')
-    job_value = fields.Monetary("Job Value", compute="_compute_job_value", currency_field='currency_id', store=True,
+    job_value = fields.Monetary("Job Value",
+                                compute="_compute_job_value",
+                                currency_field='currency_id',
+                                store=True,
                                 readonly=True)
 
     project_manager = fields.Many2one('res.users', string='Project Manager')
     start_date = fields.Date(string='Start Date')
     due_date = fields.Date(string='Due Date')
-    project_status = fields.Selection(string='Project Status', selection=project_status_list)
-    payment_status = fields.Selection(string='Payment Status', selection=payment_status_list)
+    project_status = fields.Selection(string='Project Status',
+                                      selection=project_status_list)
+    payment_status = fields.Selection(string='Payment Status',
+                                      selection=payment_status_list)
 
     @api.model
     def create(self, vals):
@@ -72,13 +96,16 @@ class MercTransProjects(models.Model):
     @api.depends('volume', 'sale_rate_per_work_unit', 'discount')
     def _compute_job_value(self):
         for project in self:
-            project.job_value = (100 - project.discount) / 100 * project.volume * project.sale_rate_per_work_unit
+            project.job_value = (
+                100 - project.discount
+            ) / 100 * project.volume * project.sale_rate_per_work_unit
 
     @api.constrains('start_date', 'due_date')
     def date_constrains(self):
         for project in self:
             if project.due_date < project.start_date:
-                raise ValidationError('Due date must be greater than Start date!')
+                raise ValidationError(
+                    'Due date must be greater than Start date!')
 
 
 class MercTransInvoices(models.Model):
@@ -86,29 +113,38 @@ class MercTransInvoices(models.Model):
     _rec_name = 'invoice_name'
     _description = 'MercTrans Invoices for Project Managers'
 
-    status_list = [('invoiced', 'Invoiced'), ('paid', 'Paid'), ('unpaid', 'Unpaid')]
+    status_list = [('invoiced', 'Invoiced'), ('paid', 'Paid'),
+                   ('unpaid', 'Unpaid')]
 
     invoice_id = fields.Integer('Invoice ID')
     invoice_name = fields.Char('Invoice name')
     invoice_date = fields.Date(string='Invoice Date')
-    invoice_client = fields.Many2one('res.partner', string='Client', required='True')
-    invoice_details_ids = fields.Many2many('merctrans.projects', string='Invoice Lines')
+    invoice_client = fields.Many2one('res.partner',
+                                     string='Client',
+                                     required='True')
+    invoice_details_ids = fields.Many2many('merctrans.projects',
+                                           string='Invoice Lines')
     currency_id = fields.Many2one('res.currency', string='Currency')
-    invoice_value = fields.Monetary("Invoice Value", compute="_compute_invoice_value", currency_field='currency_id')
+    invoice_value = fields.Monetary("Invoice Value",
+                                    compute="_compute_invoice_value",
+                                    currency_field='currency_id')
     # invoice_details_ids = fields.Many2many('merctrans.invoices.lines', 'job_id', string="Invoice Lines")
-    invoice_status = fields.Selection(string="Invoice Status", selection=status_list)
+    invoice_status = fields.Selection(string="Invoice Status",
+                                      selection=status_list)
 
     @api.depends('invoice_details_ids')
     def _compute_invoice_value(self):
         for item in self:
-            item.invoice_value = sum(x.job_value for x in item.invoice_details_ids)
+            item.invoice_value = sum(x.job_value
+                                     for x in item.invoice_details_ids)
 
     @api.constrains('invoice_details_ids')
     def currency_constrains(self):
         for job in self:
             for x in job.invoice_details_ids:
                 if job.currency_id != x.currency_id:
-                    raise ValidationError('Job currency must be the same as invoice currency!')
+                    raise ValidationError(
+                        'Job currency must be the same as invoice currency!')
 
     @api.model
     def create(self, vals):
