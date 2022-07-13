97e6ed6e480fdc7a81331ad10e159481847fc068
truong tang <txt2408@gmail.com>
2022-07-14 06:14:36 +0700

    fix currencies

--- local-addons/merctrans_projects/models/merctrans_clients.py
+++ local-addons/merctrans_projects/models/merctrans_clients.py
@@ -1,22 +1,32 @@
+import re
 from odoo.exceptions import ValidationError
-
 from odoo import api, fields, models
 
 
 class MerctransClient(models.Model):
     _name = 'merctrans.clients'
     _rec_name = 'name'
-    _inherits = {'res.partner': 'partner_id'}
-    _description = "Merctrans's Partner"
+    _description = "Merctrans's Clients"
 
     # partner_id = fields.Many2one('res.partner', required=True, ondelete='restrict', auto_join=True,
     #                              string='Related Partner', help='Partner-related data of the user')
-    name = fields.Char(related='partner_id.name',
-                       inherited=True,
-                       readonly=False)
-    email = fields.Char(related='partner_id.email',
-                        inherited=True,
-                        readonly=False)
+    name = fields.Char(string='Client')
+    email = fields.Char(string='Email')
     client_note = fields.Html('Client note')
+    phone_number = fields.Char(string='Phone number')
+    website = fields.Char(string='Website')
     # client_currency = fields.Many2one('res.currency',
     #                                   string="Currency",)
+    @api.constrains('name')
+    def check_duplicate_name(self):
+        for company_rec in self:
+            company_rec = self.env['merctrans.clients'].search([('name', '=', self.name), ('id', '!=', self.id)])
+            if company_rec:
+                raise ValidationError('Company name cannot be duplicated!')
+
+    @api.constrains('email')
+    def validate_email(self):
+        if self.email:
+            match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', self.email)
+        if match == None:
+            raise ValidationError('Not a valid email')
