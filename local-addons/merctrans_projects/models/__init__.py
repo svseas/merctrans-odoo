5e87fe2e95cb14c4ac84cafaa923041f4b312eeb
hglong16 <intihad.vuong@gmail.com>
2022-07-26 12:49:38 +0700

    model contributors done

--- local-addons/merctrans_projects/models/__init__.py
+++ local-addons/merctrans_projects/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 # from . import merctrans_sales
-from . import merctrans_clients, merctrans_pos, merctrans_projects, merctrans_invoices
+from . import merctrans_clients, merctrans_pos, merctrans_projects, merctrans_invoices, merctrans_contributors
